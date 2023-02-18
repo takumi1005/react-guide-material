6f8419ea192a6be880b63994b0842c0a460cc684
takumi <takumim1005@gmail.com>
2023-02-19 00:08:27 +0900

    アロー関数の基礎

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,19 @@
 function fn(number) {
-  return number * 2;
+	return number * 2;
 }
+
+console.log(fn(2));
+
+//アロー関数
+// const fnArrow = (number) => {
+// 	return number * 2;
+// };
+
+//省略形（複数行、引数が複数ある場合は以下の書き方は出来ないよ）
+const fnArrow = (number) => number * 2;
+
+console.log(fnArrow(2));
+
+//オブジェクトを使う場合は下記
+const fnArrowObj = (number) => ({ result: number * 2 });
+console.log(fnArrowObj(2));
