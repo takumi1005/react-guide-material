668195ca8cc9b5f27df51d5ea6ccb65e3224c8dd
takumi <takumim1005@gmail.com>
2023-02-19 23:09:34 +0900

    分割代入の基礎

--- 03_js_basic/050_destructuring/start/main.js
+++ 03_js_basic/050_destructuring/start/main.js
@@ -1,26 +1,28 @@
-const arry = ["配列1", "配列2", "配列3"];
-console.log(arry[0]);
-console.log(arry[2]);
+// 変数名で呼び出せる
+const [a, b, c] = ["配列1", "配列2", "配列3"];
+console.log(a);
+console.log(b);
 
-const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
-console.log(obj.x);
-console.log(obj.y);
+// プロパティ名と一致させて呼び出せる
+const { x, z } = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+console.log(x);
+console.log(z);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
 const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
 
-const fnArr = (arry) => {
-  console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
+const fnArr = ([country, state, city]) => {
+	console.log("---配列---");
+	console.log(`country: ${country}`);
+	console.log(`state: ${state}`);
+	console.log(`city: ${city}`);
 };
 
-const fnObj = (objAddr) => {
-  console.log("---オブジェクト---");
-  console.log(`country: ${objAddr.country}`);
-  console.log(`state: ${objAddr.state}`);
-  console.log(`city: ${objAddr.city}`);
+const fnObj = ({ country, state, city }) => {
+	console.log("---オブジェクト---");
+	console.log(`country: ${country}`);
+	console.log(`state: ${state}`);
+	console.log(`city: ${city}`);
 };
 
 fnArr(arr);
