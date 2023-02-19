c0a804cd7334589bbbd4c58eac2492600ee154d5
takumi <takumim1005@gmail.com>
2023-02-19 23:35:49 +0900

    スプレッド演算子の基礎

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,25 +1,27 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
-
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+let newArr = [...arr1, ...arr2];
+let newArr1 = arr1;
+newArr.push(4);
+console.log(newArr);
 
 const obj = {
-  name: "Tom",
-  age: 22,
+	name: "Tom",
+	age: 22,
 };
 const newObj = { ...obj };
+newObj.name = "John";
 
 console.log(newObj);
 
+// 引数の数が可変のときに使う
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+restA(1, 3, 4);
 
 const restB = (n, ...argB) => console.log(argB);
-restB(1, 3, 4)
+restB(1, 3, 4);
