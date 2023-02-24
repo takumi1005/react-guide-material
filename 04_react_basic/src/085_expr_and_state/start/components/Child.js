8466e7e46c52f75b04424d5511934035f14c2b2c
takumi <takumim1005@gmail.com>
2023-02-24 23:48:41 +0900

    式と分の違い

--- 04_react_basic/src/085_expr_and_state/start/components/Child.js
+++ 04_react_basic/src/085_expr_and_state/start/components/Child.js
@@ -1,16 +1,22 @@
 /* POINT 式と文
 式：何らかの値を返すもの（変数に代入できるもの）
 文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
+jsxでは式しか書けない
 */
 
 import "./Child.css";
 
 const Child = () => {
-  return (
-    <div className="component">
-      <h3>式と文</h3>
-    </div>
-  );
+	const hello = () => "hello";
+	const a = hello();
+	console.log(a);
+	for (let i = 0; i < 5; i++) {}
+	return (
+		<div className="component">
+			<h3>式と文</h3>
+			{true ? "hello" : "bye"}
+		</div>
+	);
 };
 
 export default Child;
