0c6a06bfdcf8be57c67ec968313699c1e9ce9262
takumi <takumim1005@gmail.com>
2023-02-25 18:35:13 +0900

    childrenについて

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -1,17 +1,20 @@
-import Profile from "./components/Profile";
 import Container from "./components/Container";
+import Profile from "./components/Profile";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+	{ name: "Takashi", age: 19, country: "Japan" },
+	{ name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
-  return (
-    <div>
-      <Container title="Childrenとは？" />
-    </div>
-  );
+	return (
+		<div>
+			<Container title="Childrenとは？">
+				<Profile {...profile[0]} />
+				<Profile {...profile[1]} />
+			</Container>
+		</div>
+	);
 };
 
 export default Example;
--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,11 +1,12 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
-  return (
-    <div className="container">
-      <h3>{title}</h3>
-    </div>
-  );
+const Container = ({ title, children }) => {
+	return (
+		<div className="container">
+			<h3>{title}</h3>
+			{children}
+		</div>
+	);
 };
 
 export default Container;
