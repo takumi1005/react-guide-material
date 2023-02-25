cf93bd456a8df0faf06beee0114fdcc45bd1ccfa
takumi <takumim1005@gmail.com>
2023-02-25 23:01:31 +0900

    propsのルール

--- 04_react_basic/src/120_props_rules/start/components/Hello.js
+++ 04_react_basic/src/120_props_rules/start/components/Hello.js
@@ -1,9 +1,10 @@
 const Hello = (props) => {
-  return (
-    <div>
-      <h3>Hello {props.name}</h3>
-    </div>
-  );
+	// props.name = "Bob"; // これは出来ないよ
+	return (
+		<div>
+			<h3>Hello {props.name}</h3>
+		</div>
+	);
 };
 
 export default Hello;
