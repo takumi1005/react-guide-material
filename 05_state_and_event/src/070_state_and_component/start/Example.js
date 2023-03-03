cc4a6bdcc3469dda058b6a57fc3d95d224b724c8
takumi <takumim1005@gmail.com>
2023-03-03 23:11:03 +0900

    ステートとコンポーネントの関係

--- 05_state_and_event/src/070_state_and_component/start/Example.js
+++ 05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,20 +1,37 @@
 import { useState } from "react";
 
 const Example = () => {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount((prevstate) => prevstate + 1);
-  };
-  const countDown = () => {
-    setCount(count - 1);
-  };
-  return (
-    <>
-      <h3>カウント: {count}</h3>
-      <button onClick={countUp}>+</button>
-      <button onClick={countDown}>-</button>
-    </>
-  );
+	const [toggle, setToggle] = useState(true);
+	const toggleComponent = () => {
+		setToggle((prev) => !prev);
+	};
+	return (
+		<>
+			<button onClick={toggleComponent}>toggle</button>
+			{/* 同じツリー構造の場合 */}
+			{/* コンポーネントを一意に識別する場合にkeyを設定する。keyがないと状態を引き継いでしまう */}
+			{toggle ? <Count key="A" title="A" /> : <Count key="B" title="B" />}
+		</>
+	);
+};
+
+const Count = ({ title }) => {
+	const [count, setCount] = useState(0);
+	const countUp = () => {
+		setCount((prevstate) => prevstate + 1);
+	};
+	const countDown = () => {
+		setCount(count - 1);
+	};
+	return (
+		<>
+			<h3>
+				{title}: {count}
+			</h3>
+			<button onClick={countUp}>+</button>
+			<button onClick={countDown}>-</button>
+		</>
+	);
 };
 
 export default Example;
