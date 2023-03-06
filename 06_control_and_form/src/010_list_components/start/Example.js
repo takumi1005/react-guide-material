d06f935e1245c72220c4163ac4b1d63eb3e8a43d
takumi <takumim1005@gmail.com>
2023-03-06 23:40:10 +0900

    配列の扱い方

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -1,16 +1,27 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+	const animalList = [];
+	for (const animal of animals) {
+		animalList.push(<li>{animal}</li>);
+	}
 
-  return (
-    <>
-      <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
-    </>
-  );
+	// const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>);
+	return (
+		<>
+			<h3>配列の操作</h3>
+			<ul>
+				{/* <li>{animals[0]}</li>
+				<li>{animals[1]}</li>
+				<li>{animals[2]}</li> */}
+				{/* {animalList} */}
+				{/* {helloAnimals} */}
+				{animals.map((animal) => (
+					<li>Hello, {animal}</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default Example;
