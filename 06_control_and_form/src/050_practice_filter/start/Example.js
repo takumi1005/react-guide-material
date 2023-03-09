a989e341180f48e851108ecb2ff3574d5597133b
takumi <takumim1005@gmail.com>
2023-03-09 23:51:36 +0900

    練習問題done

--- 06_control_and_form/src/050_practice_filter/start/Example.js
+++ 06_control_and_form/src/050_practice_filter/start/Example.js
@@ -1,37 +1,48 @@
+import { useState } from "react";
 import Profile from "./components/Profile";
 
 const persons = [
-  {
-    name: "Geo",
-    age: 18,
-    hobbies: ["sports", "music"],
-  },
-  {
-    name: "Tom",
-    age: 25,
-    hobbies: ["movie", "music"],
-  },
-  {
-    name: "Lisa",
-    age: 21,
-    hobbies: ["sports", "travel", "game"],
-  },
+	{
+		name: "Geo",
+		age: 18,
+		hobbies: ["sports", "music"],
+	},
+	{
+		name: "Tom",
+		age: 25,
+		hobbies: ["movie", "music"],
+	},
+	{
+		name: "Lisa",
+		age: 21,
+		hobbies: ["sports", "travel", "game"],
+	},
 ];
 
 const Example = () => {
-  return (
-    <>
-      <h3>練習問題</h3>
-      <p>入力欄を設置して、入力値と名前が一致したもののみ表示する仕組みを作成してください。</p>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            <Profile {...person} />
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+	const [filterVal, setFilterVal] = useState("");
+	return (
+		<>
+			<h3>練習問題</h3>
+			<p>
+				入力欄を設置して、入力値と名前が一致したもののみ表示する仕組みを作成してください。
+			</p>
+			<input
+				type="text"
+				value={filterVal}
+				onChange={(e) => setFilterVal(e.target.value)}
+			/>
+			<ul>
+				{persons
+					.filter((person) => person.name.indexOf(filterVal) !== -1)
+					.map((person) => (
+						<li key={person.name}>
+							<Profile {...person} />
+						</li>
+					))}
+			</ul>
+		</>
+	);
 };
 
 export default Example;
