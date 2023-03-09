da3535686c5a63b65e54c8560acd1770917c4402
takumi <takumim1005@gmail.com>
2023-03-09 23:36:09 +0900

    配列のフィルターメソッドの使い方

--- 06_control_and_form/src/040_list_and_filter/start/Example.js
+++ 06_control_and_form/src/040_list_and_filter/start/Example.js
@@ -3,17 +3,24 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-  return (
-    <>
-      <h3>配列のフィルター</h3>
-      <ul>
-        {animals
-          .map((animal) => (
-          <li>{animal}</li>
-        ))}
-      </ul>
-    </>
-  );
+	const [filterVal, setFilterVal] = useState("");
+	return (
+		<>
+			<h3>配列のフィルター</h3>
+			<input
+				type="text"
+				value={filterVal}
+				onChange={(e) => setFilterVal(e.target.value)}
+			/>
+			<ul>
+				{animals
+					.filter((animal) => animal.indexOf(filterVal) !== -1)
+					.map((animal) => (
+						<li key={animal}>{animal}</li>
+					))}
+			</ul>
+		</>
+	);
 };
 
 export default Example;
