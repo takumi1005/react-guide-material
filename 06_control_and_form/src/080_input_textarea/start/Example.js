2c783ec77bc0763aa54ba019bbbcafe3b4b632c0
takumi <takumim1005@gmail.com>
2023-03-18 00:33:50 +0900

    inputとtextarea

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
+	const [val, setVal] = useState("");
+	const clearVal = () => setVal("");
+	return (
+		<div>
+			<label htmlFor="456">ラベル</label>
+			<div>
+				<input
+					id="123"
+					placeholder="こんにちは"
+					value={val}
+					onChange={(e) => setVal(e.target.value)}
+				/>
+				<textarea
+					id="456"
+					placeholder="こんにちは"
+					value={val}
+					onChange={(e) => setVal(e.target.value)}
+				/>
+			</div>
+			<h3>{val}</h3>
+			<button onClick={clearVal}>クリア</button>
+		</div>
+	);
 };
 
 export default Example;
