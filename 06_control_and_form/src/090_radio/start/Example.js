e19255bf96b7532a8a679ad68146cafff352c0d2
takumi <takumim1005@gmail.com>
2023-03-18 10:37:40 +0900

    ラジオボタン

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
+	const [fruit, setFruit] = useState("Apple");
+	const onChange = (e) => setFruit(e.target.value);
+	const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+	return (
+		<div>
+			{RADIO_COLLECTION.map((value) => {
+				return (
+					<label key={value}>
+						<input
+							type="radio"
+							value={value}
+							checked={fruit === value}
+							onChange={onChange}
+						/>
+						{value}
+					</label>
+				);
+			})}
+			<h3>私は{fruit}が食べたい</h3>
+		</div>
+	);
 };
 
 export default Example;
