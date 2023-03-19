10ada9e7969c691028d5c290693116bf49d8246a
takumi <takumim1005@gmail.com>
2023-03-19 23:08:56 +0900

    プルダウンの作成方法

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
+// POINT プルダウンの実装
 const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
+	const [selected, setSelected] = useState("Banana");
+
+	const OPTIONS = ["Apple", "Banana", "Cherry"];
+
+	return (
+		<>
+			<select value={selected} onChange={(e) => setSelected(e.target.value)}>
+				{OPTIONS.map((opt) => (
+					<option key={opt} value={opt}>
+						{opt}
+					</option>
+				))}
+				{/* <option value="Apple">Apple</option>
+        <option value="Banana">Banana</option>
+        <option value="Cherry">Cherry</option> */}
+			</select>
+			<div>選択された果物：{selected}</div>
+		</>
+	);
 };
 
 export default Example;
