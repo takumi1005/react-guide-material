0f39f439bf6e192b2bca0bd566d6242e1a0e0a30
takumi <takumim1005@gmail.com>
2023-03-20 22:42:18 +0900

    todoを作ってみる

--- 06_control_and_form/src/130_reminder/start/Example.js
+++ 06_control_and_form/src/130_reminder/start/Example.js
@@ -1,10 +1,12 @@
+import Todo from "./components/Todo";
+
 const Example = () => {
-  return (
-    <>
-      <h3>完成系と同じような機能を作成してください。</h3>
-      <p>また、枠線で囲まれた部品をコンポーネントに分離してください。</p>
-    </>
-  );
+	return (
+		<>
+			<h2>Reminder</h2>
+			<Todo />
+		</>
+	);
 };
 
 export default Example;
