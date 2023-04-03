303a0d3da6cd482cefa3738859103efab504611a
takumi <takumim1005@gmail.com>
2023-04-03 21:53:20 +0900

    外部CSSのimportを使ったスタイリング

--- 07_styling_component/src/020_import_css/start/Example.js
+++ 07_styling_component/src/020_import_css/start/Example.js
@@ -1,32 +1,26 @@
 import { useState } from "react";
+import "./Example.css";
+import SubButton from "./components/SubButton";
 
 const Example = () => {
-  const [isSelected, setIsSelected] = useState(false);
+	const [isSelected, setIsSelected] = useState(false);
 
-  const clickHandler = () => setIsSelected((prev) => !prev);
+	const clickHandler = () => setIsSelected((prev) => !prev);
 
-  const style = {
-    margin: "auto",
-    "border-radius": "9999px",
-    border: "none",
-    display: "block",
-    width: 120,
-    height: 60,
-    fontWeight: "bold",
-    cursor: "pointer",
-    backgroundColor: isSelected ? "pink" : "",
-  };
-
-  return (
-    <>
-      <button style={style} onClick={clickHandler}>
-        ボタン
-      </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<button
+				className={`btn ${isSelected ? "selected" : ""}`}
+				onClick={clickHandler}
+			>
+				ボタン
+			</button>
+			<SubButton />
+			<div style={{ textAlign: "center" }}>
+				{isSelected && "クリックされました。"}
+			</div>
+		</>
+	);
 };
 
 export default Example;
