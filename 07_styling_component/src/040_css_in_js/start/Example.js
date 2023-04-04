8c11b094875a86201bc1848dba4d95eaf4fbe167
takumi <takumim1005@gmail.com>
2023-04-04 22:10:08 +0900

    styled-componentsを使ったスタイリング

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,4 +1,32 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+	margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  cursor: pointer;
+	background: ${({isSelected}) => isSelected ? 'pink' : ''};
+	@media (max-width: 600px) {
+		border-radius: 0;
+	}
+`;
+
+const OrangeButton = styled(StyledButton)`
+	background-color: orange;
+
+	:hover, :active {
+		color: red;
+		opacity: 0.7;
+	}
+	span {
+		font-size: 2em;
+	}
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,6 +35,8 @@ const Example = () => {
 
   return (
     <>
+			<StyledButton isSelected={isSelected} onClick={clickHandler}>ボタン</StyledButton>
+			<OrangeButton isSelected={isSelected} onClick={clickHandler}><span>ボタン</span></OrangeButton>
       <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
