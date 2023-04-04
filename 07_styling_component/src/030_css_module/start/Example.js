ad1def169b699fbd15a1818813da7239de5cb3dd
takumi <takumim1005@gmail.com>
2023-04-04 21:49:07 +0900

    CSS Modulesを使ったスタイリング

--- 07_styling_component/src/030_css_module/start/Example.js
+++ 07_styling_component/src/030_css_module/start/Example.js
@@ -1,24 +1,27 @@
 import { useState } from "react";
 
 import SubButton from "./components/SubButton";
-import "./Example.css";
+import styles from "./Example.module.css";
 
 const Example = () => {
-  const [isSelected, setIsSelected] = useState(false);
+	const [isSelected, setIsSelected] = useState(false);
 
-  const clickHandler = () => setIsSelected((prev) => !prev);
+	const clickHandler = () => setIsSelected((prev) => !prev);
 
-  return (
-    <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
-        ボタン
-      </button>
-      <SubButton />
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<button
+				className={`${styles.btn} ${isSelected ? styles.selected : ""}`}
+				onClick={clickHandler}
+			>
+				ボタン
+			</button>
+			<SubButton />
+			<div style={{ textAlign: "center" }}>
+				{isSelected && "クリックされました。"}
+			</div>
+		</>
+	);
 };
 
 export default Example;
