2d8b157f174cd76cf63db16d22882f5bf1d3748b
takumi <takumim1005@gmail.com>
2023-04-05 22:25:09 +0900

    chakra-uiを使ってみよう

--- 07_styling_component/src/050_chakra_ui/start/Example.js
+++ 07_styling_component/src/050_chakra_ui/start/Example.js
@@ -1,10 +1,12 @@
-import Todo from "./components/Todo"
+import { ChakraProvider } from "@chakra-ui/react";
+import Todo from "./components/Todo";
 
 const Example = () => {
   return (
     <>
-      <h2>Reminder</h2>
-      <Todo />
+			<ChakraProvider>
+				<Todo />
+			</ChakraProvider>
     </>
   );
 };
--- 07_styling_component/src/050_chakra_ui/start/components/Form.js
+++ 07_styling_component/src/050_chakra_ui/start/components/Form.js
@@ -1,31 +1,69 @@
+import { Button, HStack, Input, useToast } from "@chakra-ui/react";
 import { useState } from "react";
+
 const Form = ({ createTodo }) => {
-  const [enteredTodo, setEnteredTodo] = useState("");
+	const [enteredTodo, setEnteredTodo] = useState("");
 
-  const addTodo = (e) => {
-    e.preventDefault();
+	const toast = useToast();
 
-    const newTodo = {
-      id: Math.floor(Math.random() * 1e5),
-      content: enteredTodo,
-    };
+	const addTodo = (e) => {
+		e.preventDefault();
 
-    createTodo(newTodo);
+		if (!enteredTodo) {
+			toast({
+				title: "新しいタスクを入力してください",
+				status: "error",
+				duration: 2000,
+				isClosable: true,
+			});
+			return;
+		}
 
-    setEnteredTodo("");
-  };
-  return (
-    <div>
-      <form onSubmit={addTodo}>
-        <input
-          type="text"
-          value={enteredTodo}
-          onChange={(e) => setEnteredTodo(e.target.value)}
-        />
-        <button>追加</button>
-      </form>
-    </div>
-  );
+		const newTodo = {
+			id: Math.floor(Math.random() * 1e5),
+			content: enteredTodo,
+		};
+
+		createTodo(newTodo);
+
+		setEnteredTodo("");
+
+		toast({
+			title: "新しいタスクを追加しました！",
+			description: enteredTodo,
+			status: "info",
+			duration: 3000,
+			isClosable: true,
+		});
+	};
+
+	return (
+		<form onSubmit={addTodo}>
+			<HStack>
+				{/* POINT ChakraのInputコンポーネントを使う  */}
+				<Input
+					placeholder="新しいタスク"
+					_placeholder={{ opacity: "0.3", color: "gray.500" }}
+					size="lg"
+					p={3}
+					bgColor="white"
+					variant="flushed"
+					value={enteredTodo}
+					onChange={(e) => setEnteredTodo(e.target.value)}
+				/>
+				<Button
+					colorScheme="blue"
+					size="md"
+					bgColor="white"
+					variant="outline"
+					px={7}
+					type="submit"
+				>
+					追加
+				</Button>
+			</HStack>
+		</form>
+	);
 };
 
 export default Form;
